4d5f19ef5da3f9cda8bbc5e8e287c69db1533b94
Anselm Brehme <development.anselm@breh.me>
2018-01-07 22:20:29 +0100

    done with vanilla

--- src/main/java/org/spongepowered/common/event/SpongeCommonEventFactory.java
+++ src/main/java/org/spongepowered/common/event/SpongeCommonEventFactory.java
@@ -1098,7 +1098,7 @@ public class SpongeCommonEventFactory {
         CraftItemEvent.Preview event = SpongeEventFactory
                 .createCraftItemEventPreview(Sponge.getCauseStackManager().getCurrentCause(), inventory, previewTransaction, Optional.ofNullable(recipe), ((Inventory) container), transactions);
         SpongeImpl.postEvent(event);
-        PacketPhaseUtil.handleSlotRestore(player, container, transactions, event.isCancelled());
+        PacketPhaseUtil.handleSlotRestore(player, container, new ArrayList<>(transactions), event.isCancelled());
         return event;
     }
 
@@ -1108,7 +1108,8 @@ public class SpongeCommonEventFactory {
                 .createCraftItemEventCraft(Sponge.getCauseStackManager().getCurrentCause(), result, inventory, Optional.ofNullable(recipe), ((Inventory) container), transactions);
              SpongeImpl.postEvent(event);
         ((IMixinContainer) container).setCaptureInventory(false);
-        PacketPhaseUtil.handleSlotRestore(layer, container, transactions, event.isCancelled());
+        PacketPhaseUtil.handleSlotRestore(layer, container, new ArrayList<>(transactions), event.isCancelled());
+        transactions.clear();
         ((IMixinContainer) container).setCaptureInventory(true);
         return event;
     }
