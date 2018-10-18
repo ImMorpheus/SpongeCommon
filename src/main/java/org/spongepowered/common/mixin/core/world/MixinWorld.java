1d71dc4729cab7f79363a0e41a153046a37a4159
ImMorpheus <ImMorpheus@users.noreply.github.com>
2018-10-18 04:22:21 +0200

    Trying to improve raytracing

--- src/main/java/org/spongepowered/common/mixin/core/world/MixinWorld.java
+++ src/main/java/org/spongepowered/common/mixin/core/world/MixinWorld.java
@@ -34,6 +34,7 @@ import com.flowpowered.math.vector.Vector3i;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.crash.CrashReport;
 import net.minecraft.crash.CrashReportCategory;
@@ -60,6 +61,8 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.EnumSkyBlock;
@@ -144,6 +147,7 @@ import org.spongepowered.common.mixin.tileentityactivation.MixinWorldServer_Tile
 import org.spongepowered.common.util.SpongeHooks;
 import org.spongepowered.common.util.VecHelper;
 import org.spongepowered.common.world.SpongeDimension;
+import org.spongepowered.common.world.WorldUtil;
 import org.spongepowered.common.world.extent.ExtentViewDownsize;
 import org.spongepowered.common.world.extent.worker.SpongeMutableBiomeVolumeWorker;
 import org.spongepowered.common.world.extent.worker.SpongeMutableBlockVolumeWorker;
@@ -1447,6 +1451,181 @@ public abstract class MixinWorld implements World, IMixinWorld {
             return false;
         } finally {
             blockpos$pooledmutableblockpos.release();
+        }
+    }
+
+    @Overwrite
+    @Nullable
+    public RayTraceResult rayTraceBlocks(Vec3d vec31, Vec3d vec32, boolean stopOnLiquid, boolean ignoreBlockWithoutBoundingBox, boolean returnLastUncollidableBlock) {
+        if (!Double.isNaN(vec31.x) && !Double.isNaN(vec31.y) && !Double.isNaN(vec31.z)) {
+            if (!Double.isNaN(vec32.x) && !Double.isNaN(vec32.y) && !Double.isNaN(vec32.z)) {
+
+                int i = MathHelper.floor(vec32.x);
+                int j = MathHelper.floor(vec32.y);
+                int k = MathHelper.floor(vec32.z);
+
+                int l = MathHelper.floor(vec31.x);
+                int i1 = MathHelper.floor(vec31.y);
+                int j1 = MathHelper.floor(vec31.z);
+
+                BlockPos blockpos = new BlockPos(l, i1, j1);
+
+                // Sponge start - cache the chunk to avoid repetitive lookup later
+                net.minecraft.world.chunk.Chunk chunk = null;
+                IBlockState iblockstate;
+                if (((IMixinBlockPos) blockpos).isInvalidYPosition()) {
+                    iblockstate = Blocks.AIR.getDefaultState();
+                } else {
+                    chunk = this.getChunk(blockpos);
+                    iblockstate = chunk.getBlockState(blockpos);
+                }
+                // Sponge end
+
+                Block block = iblockstate.getBlock();
+
+                if ((!ignoreBlockWithoutBoundingBox || iblockstate.getCollisionBoundingBox(WorldUtil.asNative(this), blockpos) != Block.NULL_AABB) && block.canCollideCheck(iblockstate, stopOnLiquid)) {
+                    RayTraceResult raytraceresult = iblockstate.collisionRayTrace(WorldUtil.asNative(this), blockpos, vec31, vec32);
+
+                    if (raytraceresult != null) {
+                        return raytraceresult;
+                    }
+                }
+
+                RayTraceResult raytraceresult2 = null;
+                int k1 = 200;
+
+                // Sponge start - avoid checking blockpos more than once
+                Set<BlockPos> set = new HashSet<>();
+                // Sponge end
+
+                while (k1-- >= 0) {
+                    if (Double.isNaN(vec31.x) || Double.isNaN(vec31.y) || Double.isNaN(vec31.z)) {
+                        return null;
+                    }
+
+                    if (l == i && i1 == j && j1 == k) {
+                        return returnLastUncollidableBlock ? raytraceresult2 : null;
+                    }
+
+                    boolean flag2 = true;
+                    boolean flag = true;
+                    boolean flag1 = true;
+                    double d0 = 999.0D;
+                    double d1 = 999.0D;
+                    double d2 = 999.0D;
+
+                    if (i > l) {
+                        d0 = (double)l + 1.0D;
+                    } else if (i < l) {
+                        d0 = (double)l + 0.0D;
+                    } else {
+                        flag2 = false;
+                    }
+
+                    if (j > i1) {
+                        d1 = (double)i1 + 1.0D;
+                    } else if (j < i1) {
+                        d1 = (double)i1 + 0.0D;
+                    } else {
+                        flag = false;
+                    }
+
+                    if (k > j1) {
+                        d2 = (double)j1 + 1.0D;
+                    } else if (k < j1) {
+                        d2 = (double)j1 + 0.0D;
+                    } else {
+                        flag1 = false;
+                    }
+
+                    double d3 = 999.0D;
+                    double d4 = 999.0D;
+                    double d5 = 999.0D;
+                    double d6 = vec32.x - vec31.x;
+                    double d7 = vec32.y - vec31.y;
+                    double d8 = vec32.z - vec31.z;
+
+                    if (flag2) {
+                        d3 = (d0 - vec31.x) / d6;
+                    }
+
+                    if (flag) {
+                        d4 = (d1 - vec31.y) / d7;
+                    }
+
+                    if (flag1) {
+                        d5 = (d2 - vec31.z) / d8;
+                    }
+
+                    if (d3 == -0.0D) {
+                        d3 = -1.0E-4D;
+                    }
+
+                    if (d4 == -0.0D) {
+                        d4 = -1.0E-4D;
+                    }
+
+                    if (d5 == -0.0D) {
+                        d5 = -1.0E-4D;
+                    }
+
+                    EnumFacing enumfacing;
+
+                    if (d3 < d4 && d3 < d5) {
+                        enumfacing = i > l ? EnumFacing.WEST : EnumFacing.EAST;
+                        vec31 = new Vec3d(d0, vec31.y + d7 * d3, vec31.z + d8 * d3);
+                    } else if (d4 < d5) {
+                        enumfacing = j > i1 ? EnumFacing.DOWN : EnumFacing.UP;
+                        vec31 = new Vec3d(vec31.x + d6 * d4, d1, vec31.z + d8 * d4);
+                    } else {
+                        enumfacing = k > j1 ? EnumFacing.NORTH : EnumFacing.SOUTH;
+                        vec31 = new Vec3d(vec31.x + d6 * d5, vec31.y + d7 * d5, d2);
+                    }
+
+                    l = MathHelper.floor(vec31.x) - (enumfacing == EnumFacing.EAST ? 1 : 0);
+                    i1 = MathHelper.floor(vec31.y) - (enumfacing == EnumFacing.UP ? 1 : 0);
+                    j1 = MathHelper.floor(vec31.z) - (enumfacing == EnumFacing.SOUTH ? 1 : 0);
+                    blockpos = new BlockPos(l, i1, j1);
+
+                    // Sponge start
+                    if (!set.add(blockpos)) {
+                        continue;
+                    }
+                    // Sponge end
+
+
+                    // Sponge start - avoid chunk lookup if we're in the same chunk
+                    IBlockState iblockstate1;
+                    if (((IMixinBlockPos) blockpos).isInvalidYPosition()) {
+                        iblockstate1 = Blocks.AIR.getDefaultState();
+                    } else {
+                        if (chunk == null || !((org.spongepowered.api.world.Chunk) chunk).containsBlock(blockpos.getX(), blockpos.getY(), blockpos.getZ())) {
+                            chunk = this.getChunk(blockpos);
+                        }
+                        iblockstate1 = chunk.getBlockState(blockpos);
+                    }
+                    // Sponge end
+
+                    Block block1 = iblockstate1.getBlock();
+
+                    if (!ignoreBlockWithoutBoundingBox || iblockstate1.getMaterial() == Material.PORTAL || iblockstate1.getCollisionBoundingBox(WorldUtil.asNative(this), blockpos) != Block.NULL_AABB) {
+                        if (block1.canCollideCheck(iblockstate1, stopOnLiquid)) {
+                            RayTraceResult raytraceresult1 = iblockstate1.collisionRayTrace(WorldUtil.asNative(this), blockpos, vec31, vec32);
+
+                            if (raytraceresult1 != null) {
+                                return raytraceresult1;
+                            }
+                        } else {
+                            raytraceresult2 = new RayTraceResult(RayTraceResult.Type.MISS, vec31, enumfacing, blockpos);
+                        }
+                    }
+                }
+                return returnLastUncollidableBlock ? raytraceresult2 : null;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
         }
     }
 
